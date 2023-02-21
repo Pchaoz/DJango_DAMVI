81d9e41394bbe8d26784896aa35f358855359556
Pchaozz <polsh2001.1@gmail.com>
2023-02-21 18:38:54 +0100

    De momento me crea las migitas

--- DJango_DAMVI/settings.py
+++ DJango_DAMVI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Videojocs',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,14 @@ WSGI_APPLICATION = 'DJango_DAMVI.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'djangovideojocs',
+            'USER': 'root',
+            'PASSWORD': 'super3',
+            'HOST': 'localhost',
+            'PORT': '3306',
+     }
 }
 
 
